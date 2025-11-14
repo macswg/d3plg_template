796b93b0108a6907a169f14662e67de28424ac79
macswg <88906995+macswg@users.noreply.github.com>
2025-11-14 15:07:38 -0800

    Add designerPythonLoader to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { designerPythonLoader } from '@disguise-one/designer-pythonapi/vite-loader'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    designerPythonLoader()
+  ],
 })
